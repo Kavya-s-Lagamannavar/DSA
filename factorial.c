58b7ebd649ddf95593361b836dde46e045cce70a
Kavya-s-Lagamannavar <104971168+Kavya-s-Lagamannavar@users.noreply.github.com>
2023-07-22 08:42:35 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,5 @@
+This algorithm takes an integer n as input and calculates its factorial using a for loop. The loop starts at 1 and continues until it reaches n, multiplying result by i at each iteration. The final value of result is returned as the factorial of n.
+
+In the main() function, the user is prompted to enter a non-negative integer. If the input is negative, the program prints an error message and returns with an exit code of 1. Otherwise, the program calculates the factorial of the input and prints the result to the console.
+
+Note that this algorithm is iterative because it uses a loop to repeatedly perform a set of op
