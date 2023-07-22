2e5ab2922cb6e121c04b8f65e9596028fee38520
Kavya-s-Lagamannavar <104971168+Kavya-s-Lagamannavar@users.noreply.github.com>
2023-07-22 08:44:06 +0530

    Add files via upload

---
+++ insertion_binary.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary tree node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Pre-order traversal (Root-Left-Right)
+void preorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+// In-order traversal (Left-Root-Right)
+void inorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+// Post-order traversal (Left-Right-Root)
+void postorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    // Create the binary tree
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    // Perform different traversals
+    printf("Pre-order traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("In-order traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Post-order traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
